a8f020de3f85b1605db998e49718ddbadcf34fd1
Вячеслав <74514540+morgunovvb@users.noreply.github.com>
2022-02-06 01:06:31 +0300

    1.1

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -53,6 +53,7 @@ def post_detail(request, post_id):
     }
     return render(request, 'posts/post_detail.html', context)
 
+
 @login_required
 def add_comment(request, post_id):
     # Получите пост
@@ -64,6 +65,7 @@ def add_comment(request, post_id):
         comment.post = post
         comment.save()
     return redirect('posts:post_detail', post_id=post_id)
+
 
 @login_required
 def post_create(request):
